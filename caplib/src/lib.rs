ee52146c678e99a3958fa48f95c171a6c18a07d5
Rh4096 <rhelvetican@gmail.com>
2024-05-21 10:06:46 +0700

    adding todos

--- caplib/src/lib.rs
+++ caplib/src/lib.rs
@@ -6,3 +6,12 @@ pub enum Platform {
     Wayland,
     MacOS,
 }
+
+pub fn capture(platform: Platform) {
+    match platform {
+        Platform::Windows => todo!("Implement Windows capture."),
+        Platform::X11 => todo!("Implement X11 capture."),
+        Platform::Wayland => todo!("Implement Wayland capture."),
+        Platform::MacOS => todo!("Implement MacOS capture."),
+    }
+}
